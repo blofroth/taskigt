2f4acc498be7e7cf1bc98e1ab31df8e89769854d
Björn Löfroth <bjorn.lofroth@ffcg.se>
2018-07-01 18:09:06 +0200

    Use kind symbols, re parse on update.
     * Display pure text version of tree

--- src/lib.rs
+++ src/lib.rs
@@ -24,11 +24,44 @@ pub enum NodeKind {
     Verbatim(Option<String>)
 }
 
+impl NodeKind {
+    fn symbol(&self) -> &'static str {
+        match self {
+            Planned => "?",
+            Doing => "*",
+            Done => "#",
+            Info => "-",
+            Verbatim(_syntax) => "|"
+        }
+    }
+
+    fn parse(text: &str) -> NodeKind {
+        match &text[..1] {
+            "?" => Planned,
+            "*" => Doing,
+            "#" => Done,
+            "-" => Info,
+            "|" => Verbatim(None),
+            // should not really happen?
+            _ => Info
+        }
+    }
+}
+
 pub struct Node {
     kind: NodeKind,
     text: String,
     children_ids: Vec<NodeIdx>,
     parent: Option<NodeIdx>
+}
+
+impl Node {
+    fn display(&self) -> String {
+        let mut out = self.kind.symbol().to_string();
+        out.push(' ');
+        out.push_str(&self.text);
+        out
+    }
 }
 
 pub struct Model {
@@ -51,7 +84,7 @@ impl Component<Context> for Model {
         let mut nodes = Slab::new();
         let root_node_id = nodes.insert(Node {
             kind: Info,
-            text: "Root".to_string(),
+            text: "Task".to_string(),
             children_ids: vec![],
             parent: None
         });
@@ -63,7 +96,12 @@ impl Component<Context> for Model {
             Msg::Edit(idx, new_value) => {
                 if new_value.len() > 0 {
                     context.console.log(&format!("changed {} to {}", idx, new_value));
-                    self.nodes[idx].text = new_value;
+                    self.nodes[idx].kind = NodeKind::parse(&new_value);
+                    if new_value.len() < 3 {
+                        self.nodes[idx].text = "".to_string();
+                    } else {
+                        self.nodes[idx].text = new_value[2..].to_string();
+                    }
                 } else {
                     self.update(Msg::Delete(idx), context);
                 }
@@ -84,7 +122,7 @@ impl Component<Context> for Model {
                                               child_pos, parent_idx));
                 let new_child = self.nodes.insert(Node {
                     kind: Info,
-                    text: "Child".to_string(),
+                    text: "".to_string(),
                     children_ids: vec![],
                     parent: Some(parent_idx)
                 });
@@ -103,7 +141,7 @@ fn view_node(node: NodeIdx, nodes: &Slab<Node>) -> Html<Context, Model> {
         <li>
             <input class="node-value",
                 oninput=|e| Msg::Edit(node, e.value),
-                value=&nodes[node].text,
+                value=&nodes[node].display(),
                 onkeypress=|e| {
                        if e.key() == "Enter" { Msg::Add(node, new_idx) } else { Msg::Noop }
                }, />
@@ -113,6 +151,30 @@ fn view_node(node: NodeIdx, nodes: &Slab<Node>) -> Html<Context, Model> {
             })}
             </ul>
         </li>
+    }
+}
+
+fn build_text(level: usize, buffer: &mut String, node: &NodeIdx, nodes: &Slab<Node>) {
+    buffer.push_str(&" ".repeat(level * 2));
+    buffer.push_str(&nodes[*node].display());
+    buffer.push('\n');
+    for child_id in &nodes[*node].children_ids {
+        build_text(level + 1, buffer, child_id, nodes);
+    }
+}
+
+fn build_text_root(node: &NodeIdx, nodes: &Slab<Node>) -> String {
+    let mut buffer = String::new();
+    build_text(1, &mut buffer, node, nodes);
+    buffer
+}
+
+fn view_as_text(node: NodeIdx, nodes: &Slab<Node>) -> Html<Context, Model> {
+    html! {
+        <div>
+            <h1>{ "Export" }</h1>
+            <pre>{ build_text_root(&node, nodes) }</pre>
+        </div>
     }
 }
 
@@ -127,6 +189,7 @@ impl Renderable<Context, Model> for Model {
                         { view_node(self.root_node_id, &self.nodes) }
                     </ul>
                 </div>
+                { view_as_text(self.root_node_id, &self.nodes) }
             </div>
         }
     }
