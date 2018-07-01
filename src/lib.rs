5a93b02d5d1f61719cf852be1f9ac7db8fea8314
Björn Löfroth <bjorn.lofroth@ffcg.se>
2018-07-01 10:14:19 +0200

    Use slab as node container.
     * Prepare for deletion of nodes
     * Adding child on enter

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
-extern crate stdweb;
+#![feature(vec_remove_item)]
+
 #[macro_use]
 extern crate yew;
+extern crate slab;
 
-use stdweb::web::Date;
 use yew::prelude::*;
 use yew::services::console::ConsoleService;
 use NodeKind::*;
+use slab::Slab;
+
+// assume copy
+type NodeIdx = usize;
 
 pub enum NodeKind {
     Planned,
@@ -18,17 +23,19 @@ pub enum NodeKind {
 pub struct Node {
     kind: NodeKind,
     text: String,
-    children: Vec<Node>
+    children_ids: Vec<NodeIdx>
 }
 
 pub struct Model {
-    root_node: Node
+    root_node_id: NodeIdx,
+    nodes: Slab<Node>
 }
 
 pub enum Msg {
-    Edit(String),
-    Delete(usize),
-    Add(usize),
+    Edit(NodeIdx, String),
+    Delete(NodeIdx, NodeIdx),
+    Add(NodeIdx, usize),
+    Noop
 }
 
 impl<CTX> Component<CTX> for Model
@@ -39,40 +46,63 @@ impl<CTX> Component<CTX> for Model
     type Properties = ();
 
     fn create(_: Self::Properties, _: &mut Env<CTX, Self>) -> Self {
-        Model { root_node: Node {
+        let mut nodes = Slab::new();
+        let root_node_id = nodes.insert(Node {
             kind: Info,
             text: "Root".to_string(),
-            children: vec![]
-        }}
+            children_ids: vec![]
+        });
+        Model { root_node_id, nodes }
     }
 
     fn update(&mut self, msg: Self::Msg, context: &mut Env<CTX, Self>) -> ShouldRender {
         match msg {
-            Msg::Edit(new_value) => {
-                context.as_mut().log(&format!("changed root to {}", new_value));
-                self.root_node.text = new_value;
+            Msg::Edit(idx, new_value) => {
+                if new_value.len() > 0 {
+                    context.as_mut().log(&format!("changed {} to {}", idx, new_value));
+                    self.nodes[idx].text = new_value;
+                } else {
+                    // TODO should delete, need parent index
+                    context.as_mut().log(&format!("TODO delete {}", idx));
+                }
             }
-            Msg::Delete(_) => {
-                context.as_mut().log("del - noop for now");
+            Msg::Delete(parent_idx, child_idx) => {
+                context.as_mut().log(&format!("del - {} from {}", child_idx, parent_idx));
+                self.nodes[parent_idx].children_ids
+                    .remove_item(&child_idx);
             }
-            Msg::Add(_) => {
-                context.as_mut().log("add - noop for now");
+            Msg::Add(parent_idx, child_pos) => {
+                context.as_mut().log(&format!("add - at pos {} in node {}",
+                                              child_pos, parent_idx));
+                let new_child = self.nodes.insert(Node {
+                    kind: Info,
+                    text: "Child".to_string(),
+                    children_ids: vec![]
+                });
+                self.nodes[parent_idx].children_ids
+                    .insert(child_pos, new_child);
             },
+            Msg::Noop => {}
         }
         true
     }
 }
 
-fn view_node<CTX>(node: &Node) -> Html<CTX, Model>
+fn view_node<CTX>(node: NodeIdx, nodes: &Slab<Node>) -> Html<CTX, Model>
     where
         CTX: AsMut<ConsoleService> + 'static,
 {
+    let new_idx = nodes[node].children_ids.len();
     html! {
         <li>
-            <input class="node-value", value=node.text.clone(), />
+            <input class="node-value", value=&nodes[node].text,
+                oninput=move |e: InputData| Msg::Edit(node, e.value),
+                onkeypress=move |e: KeyData| {
+                       if e.key == "Enter" { Msg::Add(node, new_idx) } else { Msg::Noop }
+               }, />
             <ul class="nodes",>
-            { for node.children.iter().map(|child| {
-                view_node(child)
+            { for nodes[node].children_ids.iter().map(|child_id| {
+                view_node(child_id.clone(), nodes)
             })}
             </ul>
         </li>
@@ -87,13 +117,10 @@ impl<CTX> Renderable<CTX, Model> for Model
         html! {
             <div>
                 <nav class="menu",>
-                    <button onclick=|_| Msg::Edit("dude".to_string()),>{ "Dude!" }</button>
-                    <button onclick=|_| Msg::Add(0),>{ "Add" }</button>
-                    <button onclick=|_| Msg::Delete(0),>{ "Delete" }</button>
                 </nav>
                 <div>
                     <ul class="nodes",>
-                        { view_node(&self.root_node) }
+                        { view_node(self.root_node_id, &self.nodes) }
                     </ul>
                 </div>
             </div>
